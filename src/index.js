6238592a31dca2690bc8a5733cf857463f88d170
William <william.williamson.91@gmail.com>
2021-03-11 15:08:57 +0100

    riennemarche

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-import { tpl, target, getData } from "./modules/charactersretriever"
-import { myModal } from "./modules/myModal"
-import { openModal } from "./modules/openmodal"
-import { clodeBtn } from "./modules/closebuttonmodal"
-import { closeModal } from "./modules/closemodal"
-import { addImage } from "./modules/addimg"
-import { addChar } from "./modules/addchar"
-import { delData } from "./modules/delchar"
+import { tpl, target, getData } from "./modules/charactersretriever.js"
+import { myModal } from "./modules/myModal.js"
+import { btn, openModal } from "./modules/openmoda.jsl"
+import { clodeBtn } from "./modules/closebuttonmodal.js"
+import { closeModal } from "./modules/closemodal.js"
+import { addImage } from "./modules/addimg.js"
+import { addChar } from "./modules/addchar.js"
+import { delData } from "./modules/delchar.js"
 
 tpl();
 target();
 getData();
 myModal();
+btn();
 openModal();
 clodeBtn();
 closeModal();
